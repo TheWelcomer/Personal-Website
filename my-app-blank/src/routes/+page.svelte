873ddb277e50fc950d1d95c0f4276e495b261d96
Donald Winkelman <dmwink.tx@gmail.com>
2024-06-04 18:27:33 -0500

    Pushing random stuffs

---
+++ my-app-blank/src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>I don't know what I'm doing</p>
+<p>But I'm having fun</p>
+<img src="https://media.giphy.com/media/3o7TKz5v9lXfzrjUms/giphy.gif" alt="funny gif">
+<style>
+  h1 {
+    color: blue;
+  }
+  p {
+    color: red;
+  }
+</style>
