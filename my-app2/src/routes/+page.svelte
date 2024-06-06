f94cdfcdb1856dc827c7c090ca97d6ce117fa435
Donald Winkelman <dmwink.tx@gmail.com>
2024-06-05 19:25:08 -0500

    Added scroll event listener to add pipe every time the user scrolls

---
+++ my-app2/src/routes/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+	let rawNumScrolled = 0;
+	let lastNumScrolled = 0;
+	$: numScrolled = Math.floor(rawNumScrolled / 6);
+	$: if (numScrolled > lastNumScrolled) {
+		for (let i = lastNumScrolled; i < numScrolled; i++) {
+			const pipe = document.createElement('div');
+			pipe.innerHTML = '|';
+			document.getElementById('append_pipes').appendChild(pipe);
+		}
+		lastNumScrolled = numScrolled;
+	}
+</script>
+
+<div id="scroll-detection" on:wheel={() => rawNumScrolled++}>
+	<div id="append_pipes"></div>
+	<div id="num-scrolled">{numScrolled}</div>
+</div>
+<style>
+	#scroll-detection {
+		display: flex;
+		flex-direction: column;
+		align-items: left;
+		justify-content: left;
+		height: 200vh;
+	}
+	#num-scrolled {
+		font-size: 2em;
+	}
+</style>
