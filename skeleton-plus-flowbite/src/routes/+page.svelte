bb8cc6ffadbb9b78b09aa2f7b4f64423909b316a
Donald Winkelman <dmwink.tx@gmail.com>
2024-06-13 03:14:56 -0500

    skeleton.dev site added and figuring out navbar

--- skeleton-plus-flowbite/src/routes/+page.svelte
+++ skeleton-plus-flowbite/src/routes/+page.svelte
@@ -1,4 +1,7 @@
-<script>
+<script lang="ts">
+  import { LightSwitch, AppShell, Avatar, AppBar} from '@skeletonlabs/skeleton';
+	import Timeline from '$lib/Timeline/Timeline.svelte';
+	import { NavBar } from '$lib/NavBar/NavBar.svelte';
 	const timerInterval = 515;
 	const initalNumberOfChars = 0;
 	const scrollIncrement = 0.72;
@@ -41,7 +44,7 @@
 		'                 __.-\'      ^<br>' +
 		'<br>' +
 		'LET’S BUILD A LIGHTHOUSE!!!!!!!!!!!!!!<br>' +
-		'\t\t\t\tlook it’s us <):)<br>' +
+
 		'\t\t\t\toriginal scaling is 32x32 pixels<br>' +
 		'\t\t\t\t(from hat to toe I am a full 32px tall)</pre><br>'
 	let render = '|<br>|<br>|<br>|<br>|<br>|<br>|<br>|<br>|<br>|<br>|<br>|<br>|<br>|<br>|<br>|<br>▋';
@@ -53,8 +56,40 @@
 		handleScroll();
 	}
 </script>
+<NavBar />
+<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+	<svelte:fragment slot="lead">(icon)</svelte:fragment>
+	(title)
+	<svelte:fragment slot="trail">(actions)</svelte:fragment>
+</AppBar>
+<AppShell slotSidebarLeft="bg-surface-500/5 w-56 p-4">
+<svelte:fragment slot="sidebarLeft">
+	<!-- Insert the list: -->
+	<nav class="list-nav">
+		<ul>
+			<li><a href="/">Home</a></li>
+			<li><a href="/about">About</a></li>
+		</ul>
+	</nav>
+	<!-- --- -->
+</svelte:fragment>
+<div class="container mx-auto p-8 space-y-8">
+	<h1 class="h1">Hello Skeleton</h1>
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+	<section>
+		<a class="btn variant-filled-primary" href="https://kit.svelte.dev/">SvelteKit</a>
+		<a class="btn variant-filled-secondary" href="https://tailwindcss.com/">Tailwind</a>
+		<a class="btn variant-filled-tertiary" href="https://github.com/">GitHub</a>
+	</section>
+</div>
+	<Avatar src="https://i.pravatar.cc/" />
+</AppShell>
 <svelte:window bind:scrollY={scrollY} />
 <div id="view">
+	<LightSwitch />
+	<div class="p-10">
+    <Timeline />
+  </div>
 	<div id="append_pipes">
 		<pre>
 			{@html render}
