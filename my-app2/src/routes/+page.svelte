9918ff1b8d78f41989989e062fb56da35994e54a
Donald Winkelman <dmwink.tx@gmail.com>
2024-06-12 20:01:52 -0500

    daisyUI set up

--- my-app2/src/routes/+page.svelte
+++ my-app2/src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
 	const timerInterval = 515;
-	const initalNumberOfChars = 10;
-	const scrollIncrement = 0.444;
+	const initalNumberOfChars = 0;
+	const scrollIncrement = 0.488;
 	const handleScroll = async () => {
 		clearInterval(timer);
 		for (let i = 0; i < numScrolled - numScrollsHandled; i++) {
@@ -43,7 +43,7 @@
 		'LET’S BUILD A LIGHTHOUSE!!!!!!!!!!!!!!<br>' +
 		'\t\t\t\tlook it’s us <):)<br>' +
 		'\t\t\t\toriginal scaling is 32x32 pixels<br>' +
-		'\t\t\t\t(from hat to toe I am a full 32px tall)<br></pre>'
+		'\t\t\t\t(from hat to toe I am a full 32px tall)</pre><br>'
 	let render = '|<br>|<br>|<br>|<br>|<br>|<br>|<br>|<br>|<br>|<br>|<br>|<br>|<br>|<br>|<br>|<br>▋';
 	let scrollY = 0;
 	$: numScrolled = Math.floor(scrollY / scrollIncrement) + initalNumberOfChars;
@@ -55,27 +55,52 @@
 </script>
 <svelte:window bind:scrollY={scrollY} />
 <div id="view">
+	<button class="btn btn-primary">
+		daisyUI Button
+	</button>
+	<div class="card bg-base-200 w-80">
+  <div class="card-body">
+    <input placeholder="Email" class="input input-bordered" />
+    <label class="label cursor-pointer">
+      Accept terms of use
+      <input type="checkbox" class="toggle" />
+    </label>
+    <label class="label cursor-pointer">
+      Submit to newsletter
+      <input type="checkbox" class="toggle" />
+    </label>
+    <button class="btn btn-neutral">Save</button>
+  </div>
+</div>
 	<div id="append_pipes">
-		{@html render}
+		<pre>
+			{@html render}
+		</pre>
 	</div>
 	<div id="num_scrolled">{numScrolled}</div>
 	<div id="scroll">{scrollY}</div>
 </div>
 <style>
+	:global(.main::-webkit-scrollbar) {
+		width: 0.25rem
+	}
+	:global(.main::-webkit-scrollbar-track) {
+		background: #1e1e24;
+	}
+	:global(.main::-webkit-scrollbar-thumb) {
+		color: #93CAED
+	}
   :global(body) {
     background-color: #00003a;
     color: #8084ff;
-    transition: background-color 0.3s
+    transition: background-color 0.3s;
   }
 	#view {
 		display: flex;
 		flex-direction: column;
 		align-items: left;
 		justify-content: left;
-		height: 100000vh;
-	}
-	#cursor {
-		font-size: 2em;
+		height: 10000vh;
 	}
   #num_scrolled {
 		font-size: 2em;
