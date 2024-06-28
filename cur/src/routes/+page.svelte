0a071f7eb5c9bacd8ec08e41c8e579f6fce179c4
Donald Winkelman <dmwink.tx@gmail.com>
2024-06-27 21:45:40 -0500

    Bug in printing fixed

--- cur/src/routes/+page.svelte
+++ cur/src/routes/+page.svelte
@@ -8,7 +8,7 @@
 							'<div class="font-inter align-middle inline-block">' +
 								'↓ Scroll to see the text ↓' +
 							'</div>',
-			text: '@<div class="font-ibm text-sky-600 text-3xl inline-block">@' +
+			text: '@<div class="font-ibm text-sky-600 text-6xl inline-block align-middle">@' +
 					'Hallo, I\'m ' +
 					'@<div class="text-rose-600 inline-block">@' +
 					'The Welcomer!!' +
@@ -27,42 +27,42 @@
 		{
 			initial: '',
 			text: '@<span style="font-weight: bold;">@This text is bold. @<span style="text-decoration: underline; background-color: lightgray;">@This part has underline and a light gray background.@</span></span>@',
-			speed: 15,
+			speed: [15],
 		},
 		{
 			initial: '',
 			text: '@<span style="font-style: italic;">@This @<span style="color: green;">@text@</span>@ is italicized and green. @<span style="letter-spacing: 1px;">@This part has increased letter spacing.@</span></span>@',
-			speed: 20,
+			speed: [20],
 		},
 		{
 			initial: '',
 			text: '@<span style="text-decoration: underline;">@This @<span style="font-size: 20px;">@text@</span>@ is underlined. @<span style="font-style: italic;">@This part is italicized.@</span></span>@',
-			speed: 5,
+			speed: [5],
 		},
 		{
 			initial: '',
 			text: '@<span style="font-size: 20px;">@This text is 20px in size. @<span style="color: blue; text-transform: uppercase;">@This part is blue and uppercase.@</span></span>@',
-			speed: 20,
+			speed: [20],
 		},
 		{
 			initial: '',
 			text: '@<span style="background-color: yellow;">@This @<span style="font-weight: bold;">@text@</span>@ has a yellow background. @<span style="border: 1px solid black;">@This part has a black border.@</span></span>@',
-			speed: 10,
+			speed: [10],
 		},
 		{
 			initial: '',
 			text: '@<span style="letter-spacing: 2px;">@This @<span style="color: purple;">@text@</span>@ has increased letter spacing. @<span style="text-shadow: 1px 1px 2px black;">@This part has a text shadow.@</span></span>@',
-			speed: 100,
+			speed: [100],
 		},
 		{
 			initial: '',
 			text: '@<span style="text-transform: uppercase;">@This @<span style="font-weight: bold;">@text@</span>@ is uppercase. @<span style="background-color: lightblue;">@This part has a light blue background.@</span></span>@',
-			speed: 1,
+			speed: [1],
 		},
 		{
 			initial: '',
 			text: '@<span style="color: blue; font-weight: bold;">@This @<span style="text-decoration: underline; font-size: 24px;">@text@</span>@ is blue, bold, underlined, and 24px in size.@</span>@',
-			speed: 10,
+			speed: [10],
 		},
 	]
 	const handleScroll = async () => {
@@ -159,95 +159,93 @@
 </script>
 <svelte:window bind:scrollY={scrollY} />
 <!--<iframe frameborder="0" src="https://itch.io/embed/2792618" width="552" height="167"><a href="https://dwinkelmanumassedu.itch.io/pacman-clone-starter">Pacman Clone Starter by dwinkelman@umass.edu</a></iframe>-->
-<div id="view">
-	<div class="card_div">
-		<div class="card1 sticky top-28 h-96 card p-4 m-4">
-			<div class="text1 text whitespace-pre-wrap">
-				{@html render[0]}
-			</div>
-			<div class="visual1 visual">
+<div class="card_div">
+	<div class="card1 sticky top-28 h-96 card p-4 m-4">
+		<div class="text1 text whitespace-pre-wrap">
+			{@html render[0]}
+		</div>
+		<div class="visual1 visual">
 
-			</div>
 		</div>
 	</div>
-	<div class="card_div">
-		<div class="card2 sticky top-28 h-96 card p-4 m-4">
-			<div class="text1 text whitespace-pre-wrap">
-				{@html render[1]}
-			</div>
-			<div class="visual1 visual">
+</div>
+<div class="card_div">
+	<div class="card2 sticky top-28 h-96 card p-4 m-4">
+		<div class="text1 text whitespace-pre-wrap">
+			{@html render[1]}
+		</div>
+		<div class="visual1 visual">
 
-			</div>
 		</div>
 	</div>
-	<div class="card_div">
-		<div class="card3 sticky top-28 h-96 card p-4 m-4">
-			<div class="text1 text whitespace-pre-wrap">
-				{@html render[2]}
-			</div>
-			<div class="visual1 visual">
+</div>
+<div class="card_div">
+	<div class="card3 sticky top-28 h-96 card p-4 m-4">
+		<div class="text1 text whitespace-pre-wrap">
+			{@html render[2]}
+		</div>
+		<div class="visual1 visual">
 
-			</div>
 		</div>
 	</div>
-	<div class="card_div">
-		<div class="card4 sticky top-28 h-96 card p-4 m-4">
-			<div class="text1 text whitespace-pre-wrap">
-				{@html render[3]}
-			</div>
-			<div class="visual1 visual">
+</div>
+<div class="card_div">
+	<div class="card4 sticky top-28 h-96 card p-4 m-4">
+		<div class="text1 text whitespace-pre-wrap">
+			{@html render[3]}
+		</div>
+		<div class="visual1 visual">
 
-			</div>
 		</div>
 	</div>
-	<div class="card_div">
-		<div class="card5 sticky top-28 h-96 card p-4 m-4">
-			<div class="text1 text whitespace-pre-wrap">
-				{@html render[4]}
-			</div>
-			<div class="visual1 visual">
+</div>
+<div class="card_div">
+	<div class="card5 sticky top-28 h-96 card p-4 m-4">
+		<div class="text1 text whitespace-pre-wrap">
+			{@html render[4]}
+		</div>
+		<div class="visual1 visual">
 
-			</div>
 		</div>
 	</div>
-	<div class="card_div">
-		<div class="card6 sticky top-28 h-96 card p-4 m-4">
-			<div class="text1 text whitespace-pre-wrap">
-				{@html render[5]}
-			</div>
-			<div class="visual1 visual">
+</div>
+<div class="card_div">
+	<div class="card6 sticky top-28 h-96 card p-4 m-4">
+		<div class="text1 text whitespace-pre-wrap">
+			{@html render[5]}
+		</div>
+		<div class="visual1 visual">
 
-			</div>
 		</div>
 	</div>
-	<div class="card_div">
-		<div class="card7 sticky top-28 h-96 card p-4 m-4">
-			<div class="text1 text whitespace-pre-wrap">
-				{@html render[6]}
-			</div>
-			<div class="visual1 visual">
+</div>
+<div class="card_div">
+	<div class="card7 sticky top-28 h-96 card p-4 m-4">
+		<div class="text1 text whitespace-pre-wrap">
+			{@html render[6]}
+		</div>
+		<div class="visual1 visual">
 
-			</div>
 		</div>
 	</div>
-	<div class="card_div">
-		<div class="card8 sticky top-28 h-96 card p-4 m-4">
-			<div class="text1 text whitespace-pre-wrap">
-				{@html render[7]}
-			</div>
-			<div class="visual1 visual">
+</div>
+<div class="card_div">
+	<div class="card8 sticky top-28 h-96 card p-4 m-4">
+		<div class="text1 text whitespace-pre-wrap">
+			{@html render[7]}
+		</div>
+		<div class="visual1 visual">
 
-			</div>
 		</div>
 	</div>
-	<div class="card_div">
-		<div class="card9 sticky top-28 h-96 card p-4 m-4">
-			<div class="text1 text whitespace-pre-wrap">
-				{@html render[8]}
-			</div>
-			<div class="visual1 visual">
+</div>
+<div class="card_div">
+	<div class="card9 sticky top-28 h-96 card p-4 m-4">
+		<div class="text1 text whitespace-pre-wrap">
+			{@html render[8]}
+		</div>
+		<div class="visual1 visual">
 
-			</div>
 		</div>
 	</div>
 </div>
