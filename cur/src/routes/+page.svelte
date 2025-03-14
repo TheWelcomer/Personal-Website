29f3f8c3c389c9d6a845ac5902762053ffc0bb3a
Donald Winkelman <dmwink.tx@gmail.com>
2025-03-14 13:07:32 -0400

    Changing text in bio card plus a bunch of other styling changes

--- cur/src/routes/+page.svelte
+++ cur/src/routes/+page.svelte
@@ -5,98 +5,7 @@
 	// Imports
 	import { onMount } from 'svelte';
 	import '../app.postcss';
-	// Constants
-	const TO_PRINT = [
-		{
-			initial:
-					'<div class="font-ibm-bold bold text-4xl text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text4xl 2xl:text-5xl">' +
-						'↓ Scroll to see the text ↓' +
-					'</div>',
-			text:
-						'@<div class="font-ibm-bold italic text-blue-700 text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl">@' +
-							'Hallo, I\'m @<br><span class="text-rose-600">@The Welcomer!!',
-			speed: [50],
-		},
-		{
-			initial: '',
-			text:
-						'@<span class="font-ibm italic text-emerald-500 text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text4xl 2xl:text-5xl">@' +
-							'Wanderer of @<span class="text-teal-700">@systems @</span>@and @<span class="text-cyan-700">science!@</span>' +
-							'@<div class="text-right">@' +
-								'... and @<span class="text-zinc-600">@delving spots@</span>@         ' +
-								'@<br>@' +
-								'... and @<span class="text-emerald-700">@backwoods@</span>@          ' +
-								'@<br>@' +
-								'... and all things @<span class="text-blue-700">@blue ' +
-								'@<br>@',
-			speed: [10],
-		},
-		{
-			initial: '',
-			text:
-						'@<div class="font-ibm-bold italic text-blue-700 text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl justify-center">@Bio@</div>~@' +
-						'@<br>@' +
-						'@<div class="font-ibm italic text-sky-600 text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text4xl 2xl:text-5xl">@' +
-						'I\'m a @<span class="text-teal-600">@CS Major @</span>@at @<span class="text-maroon">@UMass Amherst @</span>@graduating in December 2024 with a passion ' +
-						'for exploring fields with wonderful people and inspiring works',
-			speed: [100, 10],
-		},
-		{
-			initial: '',
-			text:
-						'@<div class="font-ibm italic text-indigo-400 text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text4xl 2xl:text-5xl">@' +
-							'I\'m especially interested in the fields of @<span class="text-rose-600">@Data Science, @</span><span class="text-emerald-600">@Machine Learning @</span>@, and @<span class="text-indigo-600">@Medical Software',
-			speed: [10],
-		},
-		{
-			initial: '',
-			text:
-						'@<div class="font-ibm-bold italic text-rose-600 text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl">@Top Projects @~</div>' +
-						'<br>' +
-						'<span class="font-ibm italic text-sky-600 text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text4xl 2xl:text-5xl">' +
-						'<span class="text-yellow-300">@Pandemic Essential Goods Delivery@</span>@, @<span class="text-stone-500">@Self-Driving Car Model@</span>@, @<span class="text-teal-500">@Allergy Ordering App@</span>@, and @<span class="text-purple-600">@Crohn\'s Flare Prediction',
-			speed: [35, 10],
-		},
-		{
-			initial: '',
-			text:
-						'@<div class="font-ibm-bold italic text-yellow-300 text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl justify-center">@Care Pack ' +
-						'@<div class="font-ibm italic text-yellow-100 text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text4xl 2xl:text-5xl">' +
-						'~<br>@' +
-						'Serving @<span class="text-yellow-300">@30 @</span>@unique customers at @<span class="text-yellow-300">@less than half the cost @</span>@of Instacart, Care Pack ran from May 2020 to September 2020 across a neighborhood of @<span class="text-yellow-300">@149,000 @</span>@people',
-			speed: [50, 10],
-		},
-		{
-			initial: '',
-			text:
-					'@<div class="font-ibm-bold italic text-stone-500 text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl justify-center">@Self-Driving Car ' +
-					'@~<br>@' +
-					'@<div class="font-ibm italic text-stone-300 text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text4xl 2xl:text-5xl">' +
-					'<br>@' +
-					'Vastly improved UMass club aggregator built with the help of feature requests from @<span class="text-stone-500">@8 @</span>@students, @<span class="text-stone-500">@100% @</span>@of which I fulfilled',
-			speed: [50, 10],
-		},
-		{
-			initial: '',
-			text:
-					'@<div class="font-ibm-bold italic text-teal-600 text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl justify-center">@UMass SAFER ' +
-					'@~<br>@' +
-					'@<div class="font-ibm italic text-teal-300 text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text4xl 2xl:text-5xl">' +
-					'<br>@' +
-					'Allergy ordering app @<span class="text-teal-600">@1,200% @</span>@faster than outdated UMass SAFE. Organized meetings with UMass Dining, helping release UMass Dining app @<span class="text-teal-600">@2.0 @</span>@with my safety features included',
-			speed: [50, 10],
-		},
-		{
-			initial: '',
-			text:
-					'@<div class="font-ibm-bold italic text-purple-600 text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl justify-center">@UMass CLog ' +
-					'@~<br>@' +
-					'@<div class="font-ibm italic text-purple-300 text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text4xl 2xl:text-5xl">' +
-					'<br>@' +
-					'Helps mitigate Crohn’s flares by predicting which foods are harmful. Tester with severe Crohn’s loved the app and its potential to reduce severe pain for @<span class="text-purple-600">@200+ @</span>@students with IBD',
-			speed: [50, 10],
-		},
-	]
+	import { TO_PRINT } from './resumeInfo';
 
 	// Constants
 	const TIMER_INTERVAL = 515;
@@ -144,6 +53,11 @@
 						speed++;
 						numScrolled = Math.floor(scrollY / TO_PRINT[currentCard].speed[speed]);
 						numScrollsHandled = numScrolled;
+						charsPrinted++;
+						continue;
+					}
+					if (TO_PRINT[currentCard].text[charsPrinted] === '+') {
+						render[currentCard] += '@';
 						charsPrinted++;
 						continue;
 					}
@@ -261,7 +175,8 @@
 <!-- Bind scrollY to the window's scrollY -->
 <svelte:window bind:scrollY={scrollY} />
 
-<div class="relative">
+<!-- HTML with theme background class applied -->
+<div class="relative bg-theme-background">
 	<div class="container">
 		<div class="skipcont">
 			<!-- Cards and visuals -->
@@ -279,37 +194,37 @@
 						</div>
 					</div>
 					<div class="super_card super_card_2">
-						<div class="card card_2 sticky top-28 min-h-96 mt-4 ml-4 p-4 break-words whitespace-pre-wrap opacity-0">
+						<div class="card card_2 sticky top-28 min-h-96 mt-4 ml-4 p-4 break-words whitespace-pre-wrap">
 							{@html render[2]}
 						</div>
 					</div>
 					<div class="super_card super_card_3">
-						<div class="card card_3 sticky top-28 min-h-96 mt-4 ml-4 p-4 break-words whitespace-pre-wrap opacity-0">
+						<div class="card card_3 sticky top-28 min-h-96 mt-4 ml-4 p-4 break-words whitespace-pre-wrap">
 							{@html render[3]}
 						</div>
 					</div>
 					<div class="super_card super_card_4">
-						<div class="card card_4 sticky top-28 min-h-96 mt-4 ml-4 p-4 break-words whitespace-pre-wrap opacity-0">
+						<div class="card card_4 sticky top-28 min-h-96 mt-4 ml-4 p-4 break-words whitespace-pre-wrap">
 							{@html render[4]}
 						</div>
 					</div>
 					<div class="super_card super_card_5">
-						<div class="card card_5 sticky top-28 min-h-96 mt-4 ml-4 p-4 break-words whitespace-pre-wrap opacity-0">
+						<div class="card card_5 sticky top-28 min-h-96 mt-4 ml-4 p-4 break-words whitespace-pre-wrap">
 							{@html render[5]}
 						</div>
 					</div>
 					<div class="super_card super_card_6">
-						<div class="card card_6 sticky top-28 min-h-96 mt-4 ml-4 p-4 break-words whitespace-pre-wrap opacity-0">
+						<div class="card card_6 sticky top-28 min-h-96 mt-4 ml-4 p-4 break-words whitespace-pre-wrap">
 							{@html render[6]}
 						</div>
 					</div>
 					<div class="super_card super_card_7">
-						<div class="card card_7 sticky top-28 min-h-96 mt-4 ml-4 p-4 break-words whitespace-pre-wrap opacity-0">
+						<div class="card card_7 sticky top-28 min-h-96 mt-4 ml-4 p-4 break-words whitespace-pre-wrap">
 							{@html render[7]}
 						</div>
 					</div>
 					<div class="super_card super_card_8">
-						<div class="card card_8 sticky top-28 min-h-96 mt-4 ml-4 p-4 break-words whitespace-pre-wrap opacity-0">
+						<div class="card card_8 sticky top-28 min-h-96 mt-4 ml-4 p-4 break-words whitespace-pre-wrap">
 							{@html render[8]}
 						</div>
 					</div>
@@ -319,46 +234,46 @@
 				<div class="visuals">
 					<div class="super_visual super_visual_0">
 						<div class="visual visual_0 sticky top-28 ml-4 mr-4 animate-fadeIn">
-							<img class="h-auto max-h-86.2vh rounded-lg" src="/images/professional.jpg" alt="Photo of me in fancy clothing" />
+							<img class="h-auto max-h-86.2vh rounded-lg" src="/images/professional.jpg" alt="Professional photo" />
 						</div>
 					</div>
 					<div class="super_visual super_visual_1">
 						<div class="visual visual_1 sticky top-28 ml-4 mr-4 mt-4">
-							<img class="h-auto max-h-86.2vh rounded-lg" src="/images/chin.jpg" alt="Me looking down at a funny angle" />
+							<img class="h-auto max-h-86.2vh rounded-lg" src="/images/chin.jpg" alt="Profile photo" />
 						</div>
 					</div>
 					<div class="super_visual super_visual_2">
 						<div class="visual visual_2 sticky top-28 ml-4 mr-4 mt-4">
-							<img class="h-auto max-h-86.2vh rounded-lg" src="/images/juice.jpg" alt="Me drinking from a capri sun" />
+							<img class="h-auto max-h-86.2vh rounded-lg" src="/images/juice.jpg" alt="Casual photo" />
 						</div>
 					</div>
 					<div class="super_visual super_visual_3">
 						<div class="visual visual_3 sticky top-28 ml-4 mr-4 mt-4">
-							<img class="h-auto max-h-86.2vh rounded-lg" src="/images/snow.jpg" alt="Playing in the snow with my family" />
+							<img class="h-auto max-h-86.2vh rounded-lg" src="/images/snow.jpg" alt="Winter photo" />
 						</div>
 					</div>
 					<div class="super_visual super_visual_4">
-						<div class="visual visual_4 sticky top-28 ml-4 mr-4 mt-4 opacity-0">
-							<img class="h-auto max-h-86.2vh rounded-lg" src="/images/hack_umass_2.jpg" alt="HackUMass group" />
+						<div class="visual visual_4 sticky top-28 ml-4 mr-4 mt-4">
+							<img class="h-auto max-h-86.2vh rounded-lg" src="/images/hack_umass_2.jpg" alt="HackUMass photo" />
 						</div>
 					</div>
 					<div class="super_visual super_visual_5">
 						<div class="visual visual_5 sticky top-28 ml-4 mr-4 mt-4">
-							<img class="h-auto max-h-86.2vh rounded-lg" src="/images/hack_princeton.jpg" alt="HackPrinceton group" />
+							<img class="h-auto max-h-86.2vh rounded-lg" src="/images/hack_princeton.jpg" alt="HackPrinceton photo" />
 						</div>
 					</div>
 					<div class="super_visual super_visual_6">
-						<div class="visual visual_6 sticky top-28 ml-4 mr-4 mt-4 opacity-0">
-							<img class="h-auto max-h-86.2vh rounded-lg" src="/images/wizard.jpg" alt="Me wearing a wizard hat" />
+						<div class="visual visual_6 sticky top-28 ml-4 mr-4 mt-4">
+							<img class="h-auto max-h-86.2vh rounded-lg" src="/images/wizard.jpg" alt="Fun photo" />
 						</div>
 					</div>
 					<div class="super_visual super_visual_7">
-						<div class="visual visual_7 sticky top-28 ml-4 mr-4 mt-4 opacity-0">
-							<img class="h-auto max-h-86.2vh rounded-lg" src="/images/personal_web_wireframe.jpg" alt="Personal website wireframe" />
+						<div class="visual visual_7 sticky top-28 ml-4 mr-4 mt-4">
+							<img class="h-auto max-h-86.2vh rounded-lg" src="/images/personal_web_wireframe.jpg" alt="Project photo" />
 						</div>
 					</div>
 					<div class="super_visual super_visual_8">
-						<div class="visual visual_8 sticky video top-28 ml-4 mr-4 mt-4 overflow-hidden opacity-0">
+						<div class="visual visual_8 sticky top-28 ml-4 mr-4 mt-4">
 							<iframe class="h-96 w-full rounded-lg" src="https://www.youtube.com/embed/C0lpYQtMhu8?si=BwRp5bTAGB2_TKlG" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 						</div>
 					</div>
@@ -395,62 +310,67 @@
 		margin-left: 50vw;
 	}
 
-	/* Card lengths */
+	/* Card lengths - Adjusted for combined bio and contact card */
 	.super_card_0 {
-		height: calc(3000px + 16px);
+		height: calc(4500px + 16px); /* Intro card */
 	}
 	.super_card_1 {
-		height: calc(3000px + 16px);
+		height: calc(5000px + 16px); /* Combined bio and contact card - increased height */
 	}
 	.super_card_2 {
-		height: calc(3000px + 16px)
+		height: calc(4000px + 16px); /* Technologies section */
 	}
 	.super_card_3 {
-		height: calc(1500px + 8px);
+		height: calc(3500px + 8px); /* Education section */
 	}
 	.super_card_4 {
-		height: calc(3000px + 8px);
+		height: calc(4000px + 8px); /* Experience MassAI */
 	}
 	.super_card_5 {
-		height: calc(3000px + 8px);
+		height: calc(4000px + 8px); /* Second experience section */
 	}
 	.super_card_6 {
-		height: calc(3000px + 8px);
+		height: calc(3500px + 8px); /* Projects section */
 	}
 	.super_card_7 {
-		height: calc(3000px + 8px);
+		height: calc(3500px + 8px); /* More projects */
 	}
 	.super_card_8 {
-		height: calc(3500px + 8px);
+		height: calc(4000px + 8px); /* Awards section */
 	}
 
-	/* Visual lengths */
+	/* Visual lengths - Adjusted to match card heights */
 	.super_visual_0 {
-		height: 1500px;
+		height: 2250px; /* Intro card */
 	}
 	.super_visual_1 {
-		height: 1500px;
+		height: 2500px; /* Bio and contact */
 	}
 	.super_visual_2 {
-		height: 1500px;
+		height: 2000px; /* Technologies */
 	}
 	.super_visual_3 {
-		height: 1500px;
+		height: 1750px; /* Education */
 	}
 	.super_visual_4 {
-		height: 1500px;
+		height: 2000px; /* Experience MassAI */
 	}
 	.super_visual_5 {
-		height: 1500px;
+		height: 2000px; /* Second experience */
 	}
 	.super_visual_6 {
-		height: 1500px;
+		height: 1750px; /* Projects */
 	}
 	.super_visual_7 {
-		height: 3000px;
+		height: 1750px; /* More projects */
 	}
 	.super_visual_8 {
-		height: 3500px;
+		height: 2000px; /* Awards */
+	}
+
+	/* Ensure all visuals are visible on page load */
+	.visual {
+		opacity: 1 !important;
 	}
 
 	/* Fonts */
@@ -481,5 +401,60 @@
 	@font-face {
 		font-family: 'Ubuntu Sans Mono';
 		src: url('../lib/fonts/Ubuntu Sans Mono/UbuntuSansMono-Regular.woff2') format('woff2');
+	}
+	/* Theme CSS Variables for Light/Dark Mode */
+	/* Theme CSS Variables for Light/Dark Mode */
+	:root {
+		/* Default Light Mode Colors */
+		--color-primary: #3b82f6;      /* Blue 500 */
+		--color-accent1: #10b981;      /* Emerald 500 */
+		--color-accent2: #8b5cf6;      /* Violet 500 */
+		--color-accent3: #06b6d4;      /* Cyan 500 */
+		--color-accent4: #34d399;      /* Emerald 400 */
+		--color-accent5: #f43f5e;      /* Rose 500 */
+		--color-accent6: #a78bfa;      /* Violet 400 */
+		--color-text: #1f2937;         /* Gray 800 */
+		--color-neutral: #4b5563;      /* Gray 600 */
+		--color-link: #0ea5e9;         /* Sky 500 */
+		--color-link-border: #bae6fd;  /* Sky 200 */
+		--color-border: #e5e7eb;       /* Gray 200 */
+		--color-background: #ffffff;   /* White */
+	}
+
+	/* Dark Mode Colors - Modified to use .dark class instead of media query */
+	:root.dark, html.dark {
+		--color-primary: #60a5fa;    /* Blue 400 */
+		--color-accent1: #34d399;    /* Emerald 400 */
+		--color-accent2: #a78bfa;    /* Violet 400 */
+		--color-accent3: #22d3ee;    /* Cyan 400 */
+		--color-accent4: #6ee7b7;    /* Emerald 300 */
+		--color-accent5: #fb7185;    /* Rose 400 */
+		--color-accent6: #c4b5fd;    /* Violet 300 */
+		--color-text: #f9fafb;       /* Gray 50 */
+		--color-neutral: #d1d5db;    /* Gray 300 */
+		--color-link: #38bdf8;       /* Sky 400 */
+		--color-link-border: #0284c7; /* Sky 600 */
+		--color-border: #4b5563;     /* Gray 600 */
+		--color-background: #111827; /* Gray 900 */
+	}
+
+	/* Theme Class Utilities */
+	.text-theme-primary { color: var(--color-primary) !important; }
+	.text-theme-accent1 { color: var(--color-accent1) !important; }
+	.text-theme-accent2 { color: var(--color-accent2) !important; }
+	.text-theme-accent3 { color: var(--color-accent3) !important; }
+	.text-theme-accent4 { color: var(--color-accent4) !important; }
+	.text-theme-accent5 { color: var(--color-accent5) !important; }
+	.text-theme-accent6 { color: var(--color-accent6) !important; }
+	.text-theme-text { color: var(--color-text) !important; }
+	.text-theme-neutral { color: var(--color-neutral) !important; }
+	.text-theme-link { color: var(--color-link) !important; }
+	.border-theme-link-border { border-color: var(--color-link-border) !important; }
+	.border-theme-border { border-color: var(--color-border) !important; }
+	.bg-theme-background { background-color: var(--color-background) !important; }
+
+	/* Smooth transition between modes */
+	* {
+		transition: color 0.3s ease, background-color 0.3s ease, border-color 0.3s ease;
 	}
 </style>
