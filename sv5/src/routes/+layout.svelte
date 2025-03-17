f71bf96f9dac5d8d8a4d4cfd5100e1e3b9c136ba
Donald Winkelman <dmwink.tx@gmail.com>
2025-03-17 14:52:17 -0400

    Adding analytics

--- sv5/src/routes/+layout.svelte
+++ sv5/src/routes/+layout.svelte
@@ -2,6 +2,7 @@
     import '../app.css';
     // Import any components you need for the layout
     import { ModeWatcher } from "mode-watcher";
+    import { injectAnalytics } from '@vercel/analytics/sveltekit'
 </script>
 
 <!-- Your standard layout should simply pass through the slot content -->
